b1b9277318b5d68a59a6ad3e995b21a39ee8e787
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-07-28 18:52:17 +0200

    add state and function formatTimer

--- app/script.js
+++ app/script.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 
 const App = () => {
+  const [status, setStatus] = useState('off');
+  const [time, setTime] = useState(0);
+  const [timer, setTimer] = useState(null);
+
+  function formatTime() {
+    const min = Math.floor(time / 60);
+    const sec = time % 60;
+
+    let formatMin = min < 10 ? `0${min}` : `${min}`;
+    let formatSec = sec < 10 ? `0${sec}` : `${sec}`;
+
+    return `${formatMin} : ${formatSec}`;
+  }
+
   return (
     <div>
       <h1>Protect your eyes</h1>
-      <p>
-        According to optometrists in order to save your eyes, you should follow
-        the 20/20/20. It means you should to rest your eyes every 20 minutes for
-        20 seconds by looking more than 20 feet away.
-      </p>
-      <p>
-        This app will help you track your time and inform you when it's time to
-        rest.
-      </p>
-      <img src='./images/work.png' />
-      <img src='./images/rest.png' />
-      <div className='timer'>18:23</div>
-      <button className='btn'>Start</button>
-      <button className='btn'>Stop</button>
+      {status === 'off' && (
+        <div>
+          <p>
+            According to optometrists in order to save your eyes, you should
+            follow the 20/20/20. It means you should to rest your eyes every 20
+            minutes for 20 seconds by looking more than 20 feet away.
+          </p>
+          <p>
+            This app will help you track your time and inform you when it's time
+            to rest.
+          </p>
+        </div>
+      )}
+      {status === 'work' && <img src='./images/work.png' />}
+      {status === 'rest' && <img src='./images/rest.png' />}
+      {status !== 'off' && <div className='timer'>{formatTime()}</div>}
       <button className='btn btn-close'>X</button>
     </div>
   );
